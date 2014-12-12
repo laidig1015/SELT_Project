36416c704f36b5479964236723c9d8995f36f3ca
blaidig <brandon-laidig@uiowa.edu>
2014-12-12 15:13:50 -0600

    remove paginate from followeres and following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id]).paginate(page: params[:page], :per_page => 5)
+    @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
   end
 
